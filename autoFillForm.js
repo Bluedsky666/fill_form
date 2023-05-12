11db1c4cc82f6a466c7ecf917e474ba74f5a1ec5
Bluedsky666 <115670219+Bluedsky666@users.noreply.github.com>
2023-05-12 17:06:21 +0800

    Add files via upload

---
+++ autoFillForm.js
@@ -0,0 +1,97 @@
+// autoFillForm.js
+function autoFillForm() {
+    const inputText = prompt("请输入您的个人信息，使用 | 作为分隔符。\n格式：姓|名|地址|城市|州|邮编");
+
+    const parts = inputText.split('|');
+
+    const first_name = parts[0];
+    const last_name = parts[1];
+    const address = parts[2];
+    const city = parts[3];
+    const state = parts[4];
+    const zip_code = parts[5];
+
+    const signature = `${first_name} ${last_name}`;
+
+    document.getElementById("first_name").value = first_name;
+    document.getElementById("last_name").value = last_name;
+    document.getElementById("street_address_1").value = address;
+    document.getElementById("city").value = city;
+
+    function selectState(state) {
+        const select = document.getElementById("state");
+        for (let i = 0; i < select.options.length; i++) {
+            if (select.options[i].value === state) {
+                select.options[i].selected = true;
+                break;
+            }
+        }
+    }
+    selectState(state);
+
+    document.getElementById("zip_code").value = zip_code;
+
+    const yesBtn = document.querySelector('#purchased_vizzy_yes');
+    yesBtn.click();
+
+    document.getElementById("signature").value = signature;
+
+    const quantities = getRandomQuantity();
+
+    document.getElementById("twelve_pack_units").value = quantities.twelve_pack_quantity;
+    document.getElementById("twenty_four_pack_units").value = quantities.twenty_four_pack_quantity;
+    document.getElementById("single_can_units").value = quantities.single_can_quantity;
+
+    const userLocation = `${city} ${state}`;
+
+    document.querySelector('#product_5_1 [value="12-pack Unit"]').selected = true;
+    document.querySelector('#product_5_2 [value="24-pack Unit"]').selected = true;
+    document.querySelector('#product_5_3 [value="Single Can Unit"]').selected = true;
+
+    document.getElementById("units_5_1").value = quantities.twelve_pack_quantity;
+    document.getElementById("units_5_2").value = quantities.twenty_four_pack_quantity;
+    document.getElementById("units_5_3").value = quantities.single_can_quantity;
+
+    document.getElementById("purchase_place_5_1").value = userLocation;
+    document.getElementById("purchase_place_5_2").value = userLocation;
+    document.getElementById("purchase_place_5_3").value = userLocation;
+
+    const currentDate = new Date(2020, Math.floor(Math.random() * 12), 1);
+
+    for (let i = 1; i <= 3; i++) {
+        const monthIncrement = Math.floor(Math.random() * 4) + 2;
+        currentDate.setMonth(currentDate.getMonth() + monthIncrement);
+
+        if (currentDate > new Date(2022, 11, 31)) {
+            currentDate.setFullYear(2022, 11, 31);
+        }
+
+        const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+
+        document.getElementById(`units_5_${i}`).value = formattedDate;
+    }
+}
+
+function getRandomQuantity() {
+    let twelve_pack_quantity;
+    let twenty_four_pack_quantity;
+    let single_can_quantity;
+    let total_price;
+
+    while (true) {
+        twelve_pack_quantity = Math.floor(Math.random() * 11);
+        twenty_four_pack_quantity = Math.floor(Math.random() * 11);
+        single_can_quantity = Math.floor(Math.random() * 11);
+        total_price = 3 * twelve_pack_quantity + 5 * twenty_four_pack_quantity + 0.75 * single_can_quantity;
+
+        if (15 <= total_price && total_price <= 20 && twelve_pack_quantity > 0 && twenty_four_pack_quantity > 0 && single_can_quantity > 0) {
+            break;
+        }
+    }
+
+    return {
+        twelve_pack_quantity,
+        twenty_four_pack_quantity,
+        single_can_quantity
+    };
+}
